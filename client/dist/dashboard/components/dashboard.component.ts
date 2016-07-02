df142c653db12f368245d3dd60acdfd5f3d3e682
kieranfraser <kfraser@tcd.ie>
2016-07-02 22:08:06 +0100

    Merge kieran branch - added auth0 and landing page

--- client/dist/dashboard/components/dashboard.component.ts
+++ client/dist/dashboard/components/dashboard.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Inject, forwardRef} from '@angular/core';
+import {Component, OnInit, Inject, forwardRef, Input, ChangeDetectorRef} from '@angular/core';
 import {CORE_DIRECTIVES} from '@angular/common'
 import {HomeComponent} from "../../home/components/home.component";
 
@@ -8,14 +8,23 @@ declare var firebase: any;
 @Component({
   selector: 'dashboard-cmp',
   templateUrl: 'dashboard/templates/dashboard.html',
-  styleUrls: ['dashboard/styles/todo.css'],
+  styleUrls: ['dashboard/styles/todo.scss'],
   directives: [CORE_DIRECTIVES]
 })
 
 export class DashboardComponent implements OnInit {
 
+  userLoggedIn = false;
 
-  constructor(@Inject(forwardRef(() => HomeComponent)) private _parent:HomeComponent) {}
+  constructor(@Inject(forwardRef(() => HomeComponent)) private _parent:HomeComponent,
+              private ref: ChangeDetectorRef) {}
 
-  ngOnInit(){}
+  ngOnInit(){
+    console.log('init');
+    this.userLoggedIn = this._parent.userLoggedIn;
+  }
+
+  refresh(){
+    this.ref.detectChanges();
+  }
 }
